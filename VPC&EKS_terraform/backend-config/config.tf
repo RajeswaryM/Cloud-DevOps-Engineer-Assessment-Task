752c50b068e2ae77ec411e4e76d60e31a176199e
RajeswaryM <91729114+RajeswaryM@users.noreply.github.com>
2024-12-30 23:11:41 +0530

    Add files via upload

---
+++ VPC&EKS_terraform/backend-config/config.tf
@@ -0,0 +1,5 @@
+region               = "ap-south-1"
+bucket               = "terraform-workspace-states"
+encrypt              = true
+kms_key_id           = "a536b73c-3940-47bd-94bc-f299c6bf46fb"
+workspace_key_prefix = "atc"
